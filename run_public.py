46d002e53cc06b50d03923a753440d86fb96cd3c
Patil Bharath Sai <144615104+PATIL-BHARATH-SAI@users.noreply.github.com>
2026-06-25 12:37:03 +0530

    Delete run_public.py

--- run_public.py
+++
@@ -1,9 +0,0 @@
-import subprocess
-
-def main():
-    print("\n🌟 Starting local Streamlit App... 🌟\n")
-    # Start the Streamlit app locally
-    subprocess.run(["streamlit", "run", "app.py"])
-
-if __name__ == "__main__":
-    main()
